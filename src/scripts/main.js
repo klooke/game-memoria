427e386e8402f80551f1a6b71b610fbf2fe9b1be
Kevin Oliveira <55069829+klooke@users.noreply.github.com>
2024-02-16 18:44:02 -0300

    talent: flip cards on click

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,17 +21,28 @@ const game = {
     "🦁",
     "🦁",
   ],
+  flipCards: [],
   field: {
     view: document.querySelector("#field"),
   },
 };
 
 
+function onCardClick(event) {
+  var elCard = event.target;
+
+  elCard.classList.add("flip");
+
+  game.flipCards.push(elCard);
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   for (var card of game.cards) {
     var elCard = document.createElement("div");
     elCard.className = "card";
     elCard.textContent = card;
+
+    elCard.onclick = onCardClick;
 
     game.field.view.appendChild(elCard);
   }
