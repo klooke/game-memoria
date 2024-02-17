7cf3dcfb55941971c58acaf65dbbde12662de981
Kevin Oliveira <55069829+klooke@users.noreply.github.com>
2024-02-17 09:42:53 -0300

    feat: time system

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,10 +22,55 @@ const game = {
     "🦁",
   ],
   flipCards: [],
+  time: {
+    view: document.querySelector("#time"),
+    id: null,
+    value: 0,
+  },
+  timeMax: {
+    view: document.querySelector("#time-max"),
+    value: 180, // in seconds
+  },
+  bestTime: {
+    view: document.querySelector("#best-time"),
+    value: 0,
+  },
   field: {
     view: document.querySelector("#field"),
   },
 };
+
+function formatSecondsToTime(seconds) {
+  seconds %= 3600;
+
+  var min = parseInt(seconds / 60);
+  var seg = seconds % 60;
+
+  min = String(min).padStart(2, "0");
+  seg = String(seg).padStart(2, "0");
+
+  return `${min}:${seg}`;
+}
+
+function updateTime() {
+  game.time.value++;
+
+  game.time.view.textContent = formatSecondsToTime(game.time.value);
+
+  if (game.time.value >= game.timeMax.value) clearInterval(game.time.id);
+}
+
+function startTime() {
+  game.timeMax.view.textContent = formatSecondsToTime(game.timeMax.value);
+  game.time.id = setInterval(updateTime, 1000);
+}
+
+function updateBestTime() {
+  if (game.bestTime.value > game.time.value) return;
+
+  game.bestTime.value = game.time.value;
+  game.bestTime.view.textContent = formatSecondsToTime(game.bestTime.value);
+}
 
 function shuffle(array) {
   var newArray = [];
@@ -79,4 +124,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
     game.field.view.appendChild(elCard);
   }
+
+  startTime();
 });
